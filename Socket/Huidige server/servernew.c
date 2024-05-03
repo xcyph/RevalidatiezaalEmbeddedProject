f3efb7af46d7fda70064ae7265ee6590434a02da
Dialpr <125963156+Dialpr@users.noreply.github.com>
2024-05-03 15:19:33 +0200

    Update servernew.c
    - Auto format in visual studio uitgevoerd

--- Socket/Huidige server/servernew.c
+++ Socket/Huidige server/servernew.c
@@ -19,114 +19,114 @@
 char bufferDeur[5] = "open";
 char bufferLCD[8];
 
-void stuurnaarLCD(char hartslag, char zuurstof){
-    int fd = open("/dev/i2c-1",O_RDWR);
-    ioctl(fd,I2C_SLAVE,I2C_ADDR_LCD);
-    sprintf(bufferLCD, "%d %d", hartslag, zuurstof);
-    write(fd, bufferLCD, TX_BUFFERSIZELCD);
-    printf("LCD sturen\n");
+void stuurnaarLCD(char hartslag, char zuurstof) {
+	int fd = open("/dev/i2c-1", O_RDWR);
+	ioctl(fd, I2C_SLAVE, I2C_ADDR_LCD);
+	sprintf(bufferLCD, "%d %d", hartslag, zuurstof);
+	write(fd, bufferLCD, TX_BUFFERSIZELCD);
+	printf("LCD sturen\n");
 }
 
 void openDeur() {
-    int a = 0;
-	int fd = open("/dev/i2c-1",O_RDWR);
-	ioctl(fd,I2C_SLAVE,I2C_ADDR_DEUR);
-    write(fd,bufferDeur,TX_BUFFERSIZEDEUR);
-    printf("Deur open\n");
+	int a = 0;
+	int fd = open("/dev/i2c-1", O_RDWR);
+	ioctl(fd, I2C_SLAVE, I2C_ADDR_DEUR);
+	write(fd, bufferDeur, TX_BUFFERSIZEDEUR);
+	printf("Deur open\n");
 }
 
 int main(int argc, char const* argv[]) {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    socklen_t addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
+	int server_fd, new_socket;
+	struct sockaddr_in address;
+	int opt = 1;
+	socklen_t addrlen = sizeof(address);
+	char buffer[1024] = { 0 };
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-        &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+		&opt, sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr*)&address,
-        sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-while (1) {
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
+	if (bind(server_fd, (struct sockaddr*)&address,
+		sizeof(address)) < 0) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	while (1) {
+		if (listen(server_fd, 3) < 0) {
+			perror("listen");
+			exit(EXIT_FAILURE);
+		}
 
-    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
-    }
+		if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
 
-    int valread = read(new_socket, buffer, 1024 - 1);
-    buffer[valread] = '\0'; // Null-terminate de buffer
+		int valread = read(new_socket, buffer, 1024 - 1);
+		buffer[valread] = '\0'; // Null-terminate de buffer
 
-    if (valread > 0) {
-        int hartslag;
-            int zuurstof;
-            int encoder;
-        printf("%s\n", buffer);
-        if (sscanf(buffer, "Hartslag %d Zuurstof %d", &hartslag, &zuurstof) == 2)
-        stuurnaarLCD(hartslag, zuurstof);
-            //data naat txtfile
-        if (strstr(buffer, "Hartslag") != NULL){
-            FILE *fl = fopen("Data.txt", "w");
-            if (fl == NULL){
-                perror("File openen mislukt.");
-                exit(EXIT_FAILURE);
-            }
-            fprintf(fl, "%s", buffer);
-            fflush(fl);
-            
-            //int hartslag;
-            //double zuurstof;
-            //int encoder;
-            // Scan de buffer voor verwachte data
-            if (sscanf(buffer, "Hartslag %d Zuurstof %lf Encoder %d", &hartslag, &zuurstof, &encoder) == 3) {
-                printf("Hartslag: %d\n", hartslag);
-                printf("Zuurstof: %.1f\n", zuurstof);
-                printf("Encoder: %d\n", encoder);
-                
-            // Hier zou je de waarden kunnen opslaan of verder verwerken
-            }
-            fclose(fl);
-        } 
-        //else if(strstr(buffer, "RFID"){//afhandeling RFID
-            else if (strstr(buffer, "F7A1061B")){
-               // strcpy(bufferDeur, "open");
-                openDeur();
-            }
-           /* int RFID;
-            if (sscanf(buffer, "RFID %d ", &RFID) == 1){
-                if (strstr(RFID, "F7A1061B"){  //F7A1061B
-                    bufferDeur = "open"
-                    opendeur();
-                    bufferDeur = 
-                }
-            }
-        */
-        
-        }
-       
+		if (valread > 0) {
+			int hartslag;
+			int zuurstof;
+			int encoder;
+			printf("%s\n", buffer);
+			if (sscanf(buffer, "Hartslag %d Zuurstof %d", &hartslag, &zuurstof) == 2)
+				stuurnaarLCD(hartslag, zuurstof);
+			//data naat txtfile
+			if (strstr(buffer, "Hartslag") != NULL) {
+				FILE* fl = fopen("Data.txt", "w");
+				if (fl == NULL) {
+					perror("File openen mislukt.");
+					exit(EXIT_FAILURE);
+				}
+				fprintf(fl, "%s", buffer);
+				fflush(fl);
 
-        close(new_socket);
-    }
-    
-    close(server_fd);
-    return 0;
+				//int hartslag;
+				//double zuurstof;
+				//int encoder;
+				// Scan de buffer voor verwachte data
+				if (sscanf(buffer, "Hartslag %d Zuurstof %lf Encoder %d", &hartslag, &zuurstof, &encoder) == 3) {
+					printf("Hartslag: %d\n", hartslag);
+					printf("Zuurstof: %.1f\n", zuurstof);
+					printf("Encoder: %d\n", encoder);
+
+					// Hier zou je de waarden kunnen opslaan of verder verwerken
+				}
+				fclose(fl);
+			}
+			//else if(strstr(buffer, "RFID"){//afhandeling RFID
+			else if (strstr(buffer, "F7A1061B")) {
+				// strcpy(bufferDeur, "open");
+				openDeur();
+			}
+			/* int RFID;
+			 if (sscanf(buffer, "RFID %d ", &RFID) == 1){
+				 if (strstr(RFID, "F7A1061B"){  //F7A1061B
+					 bufferDeur = "open"
+					 opendeur();
+					 bufferDeur =
+				 }
+			 }
+		 */
+
+		}
+
+
+		close(new_socket);
+	}
+
+	close(server_fd);
+	return 0;
 }
